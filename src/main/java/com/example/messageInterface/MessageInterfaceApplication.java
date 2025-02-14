390b7cba1b7f136bc5b5ea5b0589086b8a2fb4fc
Léa Schmitt <146575913+leashmt@users.noreply.github.com>
2025-02-14 10:56:36 +0100

    initial commit

---
+++ src/main/java/com/example/messageInterface/MessageInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.messageInterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageInterfaceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/messageInterface/Model/Message.java
@@ -0,0 +1,56 @@
+package com.example.messageInterface.Model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private String author;
+
+    private LocalDateTime date;
+
+    public Message() {
+        this.date = LocalDateTime.now();
+    }
+
+    public Message(String content, String author) {
+        this.content = content;
+        this.author = author;
+        this.date = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+}
